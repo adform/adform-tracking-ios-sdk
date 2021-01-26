104fb5e7d9bc493430e391d7177c2540f8f57df6
Vladas Drejeris <vladas.d@ito.lt>
2021-01-26 17:13:54 +0200

    Update SPM package

--- Package.swift
+++ Package.swift
@@ -6,16 +6,19 @@ import PackageDescription
 let package = Package(
     name: "AdformTracking",
     platforms: [
-            .iOS(.v9)
-        ],
+        .iOS(.v9)
+    ],
     products: [
         .library(
             name: "AdformTracking",
             targets: ["AdformTracking"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/adform/protobuf-ios", .upToNextMajor(from: "3.11.5")),
+    ],
     targets: [
         .binaryTarget(
-                    name: "AdformTracking",
-                    path: "AdformTracking.xcframework")
+            name: "AdformTracking",
+            path: "AdformTracking.xcframework")
     ]
 )
